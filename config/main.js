a99783efdbe46d32a136563820c99f79503123df
Ronnie <rpanaligan@ucmerced.edu>
2018-01-12 23:32:22 -0800

    QUICK UPDATE

---
+++ config/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+    url : 'mongodb://localhost/lolreact',
+    secret: 'REPLACE THIS WITH A SECRET',
+    apiKey: 'REPLACE THIS WITH RIOT API',
+    baseUrl: 'https://na1.api.riotgames.com/lol/'
+    //url : process.env.MONGODB_URI
+};
