23e6ebfeaf1843988f960597b19fc8b5fbdc3b14
Ronnie Panaligan <rpanaligan@ucmerced.edu>
2018-01-28 01:39:57 -0800

    fix main api route

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 module.exports = {
     url : 'mongodb://localhost/lolreact',
     secret: 'REPLACE THIS WITH A SECRET',
-    apiKey: 'REPLACE THIS WITH RIOT API',
+    apiKey: 'RGAPI-be770ecb-3d6f-4792-9792-3dfb7e3031c5',
     baseUrl: 'https://na1.api.riotgames.com/lol/'
     //url : process.env.MONGODB_URI
 };
--- routes/api.js
+++ routes/api.js
@@ -6,21 +6,32 @@ const Summoner = require('../models/summoner');
 const baseURL = config.baseUrl;
 const apiKey = `?api_key=${config.apiKey}`
 
-//basic route to get summoner info
-router.get('/summoner/:summoner_name', (req, res, next) => {
+//basic route to get summoner info such as accountId
+router.post('/summoner/:summoner_name', (req, res, next) => {
   const name = req.params.summoner_name;
 
   axios.get(baseURL + 'summoner/v3/summoners/by-name/' + name + apiKey)
   .then((response) => {
-    Summoner.findOrCreate({
-      where: {
-        name: req.params.name
-      },
-      defaults: response.data
+    const lName = name.toLowerCase();
+
+    Summoner.findOne({ name: lName }, function(err, summoner) {
+      if(summoner == null) {
+        Summoner.create({
+          id: response.data.id,
+          accountId: response.data.accountId,
+          name: lName,
+          profileIconId: response.data.profileIconId,
+          summonerLevel: response.data.summonerLevel
+        }, function(err) {
+          if(err)
+            res.json(err);
+          res.json(response.data);
+        })
+      } else {
+        res.json(summoner);
+      }
     });
-    res.json(response.data);
-  });
-  .catch(next);
+  })
 });
 
 module.exports = router;
